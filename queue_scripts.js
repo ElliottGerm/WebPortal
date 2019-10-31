10f73dd38415f3a81ee0a9698151186eabe8d3f5
KyleTran <Ktran38@gmail.com>
2019-10-30 21:30:01 -0400

    index content added

---
+++ queue_scripts.js
@@ -0,0 +1,10 @@
+function createQueueEntry() {
+  var table = document.getElementById("queue-table");
+  var row = table.insertRow(-1);
+  var fName = row.insertCell(0);
+  var lName = row.insertCell(1);
+  var classNum = row.insertCell(2);
+  fName.innerHTML = document.getElementById("first").value;
+  lName.innerHTML = document.getElementById("last").value;
+  classNum.innerHTML = document.getElementById("numClass").value;
+}
